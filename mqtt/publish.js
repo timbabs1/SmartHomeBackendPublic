1263577c5cfdff4bffc76650118e3b94bea4b9a6
Gareth Holloway <hollow21@uni.coventry.ac.uk>
2020-03-27 11:11:49 +0000

    Update to the alarm websocket route.

--- mqtt/publish.js
+++ mqtt/publish.js
@@ -21,17 +21,23 @@ const client = mqtt.connect(mqttURL, options); //Connect to Broker
 
 /* Takes the target topic(String) as argument from the light route*/
 exports.publishData = async (topic, message) => {
-    let jsonMessage = JSON.parse(message)
-    console.log(jsonMessage)
-
+    console.log(message.toString())
+    let jsonMessage = {}
+    if(message.toString().length > 0 && JSON.parse(message)){
+        jsonMessage = JSON.parse(message)
+    }else{
+        console.log("No JSON Received")
+    }
     //Format of data required from front end {\"Light_status\" : <value> }.
-    if ('Light_status' in jsonMessage){ // This will send data regarding turning on or off.
+    if ('Light_status' in jsonMessage && message.toString().length > 0){ // This will send data regarding turning on or off.
+        jsonMessage = JSON.parse(message)
         topic === "302CEM/Horse/Requests/AutoLight"
         client.publish(topic, "0"); //Needs to be the data received from the front end.
         client.publish(topic, jsonMessage.Light_status.toString()); //Needs to be the data received from the front end.
         console.log(topic)
     // Format required for temp { \"Target_temperature\": \"25\" }.
-    }else if ('Target_temperature' in jsonMessage){
+    }else if ('Target_temperature' in jsonMessage && message.toString().length > 0){
+        jsonMessage = JSON.parse(message)
         client.publish("302CEM/Horse/Requests/TemperatureSensor/AutoTempManagement", "0"); //Needs to be the data received from the front end.
         client.publish("302CEM/Horse/Requests/TemperatureSensor/TargetTemperature", jsonMessage.Target_temperature.toString()); //Needs to be the data received from the front end.
         console.log(topic)
