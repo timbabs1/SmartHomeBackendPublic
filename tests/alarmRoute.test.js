301fab0288b8dc06c42a9351aae7b2b8438f4f54
Gareth Holloway <hollow21@uni.coventry.ac.uk>
2020-03-27 10:53:44 +0000

    Added initial websocket route for alarm requests.

---
+++ tests/alarmRoute.test.js
@@ -0,0 +1,10 @@
+const config = require("../database/config")
+const request = require('supertest');
+const server = require('../index');
+const socket = require('koa-websocket');
+
+describe('', () => {
+
+    //Test to go here.
+
+})
